cf45a457f4ad8b7878ba9db0434ded6c8859a6e2
juan.manso <jmanso@atsistemas.com>
2017-11-25 14:20:31 +0100

    Swagger

--- src/main/java/com/jml/training/SpringDataJPA/SpringDataConfiguration.java
+++ src/main/java/com/jml/training/SpringDataJPA/SpringDataConfiguration.java
@@ -1,17 +1,35 @@
 package com.jml.training.SpringDataJPA;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.google.common.base.Predicates;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @ComponentScan("com.jml.training")
 @EntityScan("com.jml.training.model")
 @EnableJpaRepositories("com.jml.training.repository")
 @EnableTransactionManagement
+@EnableSwagger2
 public class SpringDataConfiguration {
-
-
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())            
+          .paths(Predicates.not(PathSelectors.regex("/error")))                          
+          .build();                                           
+    }
+	
 }
